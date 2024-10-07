cdd8b00a5e159b87f29ecf158dbc0b47ad43494d
KARRI VAMSI KRISHNA <125668360+kvcops@users.noreply.github.com>
2024-10-07 19:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,14 @@ genai.configure(api_key=api_key)
 #session key
 
 user_data = {}  # Dictionary to store user data
-
+FIREBASE_CONFIG = {
+    "apiKey": os.environ.get("FIREBASE_API_KEY"),
+    "authDomain": os.environ.get("FIREBASE_AUTH_DOMAIN"),
+    "projectId": os.environ.get("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.environ.get("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.environ.get("FIREBASE_APP_ID"),
+}
 
 FIREBASE_TYPE = os.environ.get("FIREBASE_TYPE")
 FIREBASE_PROJECT_ID = os.environ.get("FIREBASE_PROJECT_ID")
